79ac038c3e3f3f86cdb8e8ad699abde7e3e5f558
Aubrey Plenos <breeplenos@gmail.com>
2024-03-22 12:45:13 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,6 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-import pandas as pd
-import snowflake as sf
-from snowflake import connector
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
